ec6f2c7fca14a3cdf82442d12bd44e28c611bc74
Vasily Korytov <vasily@codefresh.io>
2020-04-24 15:05:00 +0300

    separate metrics for target and ready

--- README.md
+++ README.md
@@ -3,16 +3,21 @@
 k8s provides a `cluster-autoscaler-status` configmap. sadly there was no way to easily export this data to Prometheus.  
 now there is.
 
-we export two metrics per pool:
-* `cloudProviderTarget`
-* `maxSize`
+we export three metrics per pool:
+* number of `ready` nodes
+* `cloudProviderTarget` number
+* `maxSize` number
 
 ## Example output
 ```
-# HELP cluster_autoscaler_pool cloudProviderTarget
+# HELP cluster_autoscaler_pool ready
 # TYPE cluster_autoscaler_pool gauge
 cluster_autoscaler_pool{name="csi"} 0.0
 cluster_autoscaler_pool{name="engines"} 1.0
+# HELP cluster_autoscaler_pool_target cloudProviderTarget
+# TYPE cluster_autoscaler_pool_target gauge
+cluster_autoscaler_pool_target{name="csi"} 0.0
+cluster_autoscaler_pool_target{name="engines"} 1.0
 # HELP cluster_autoscaler_pool_max maxSize
 # TYPE cluster_autoscaler_pool_max gauge
 cluster_autoscaler_pool_max{name="csi"} 10.0
--- app.py
+++ app.py
@@ -8,10 +8,12 @@ from prometheus_client import start_http_server, Gauge
 def query_metrics():
     try:
         config_map = v1.read_namespaced_config_map('cluster-autoscaler-status', 'kube-system').data['status']
-        pools = re.findall(r"Name:\s+(\S+)\n\s+Health:.+ cloudProviderTarget=(\d+) .+ maxSize=(\d+)\)\)\n", config_map, flags=re.M)
+        pools = re.findall(r"Name:\s+(\S+)\n\s+Health:.+\(ready=(\d+) .+ cloudProviderTarget=(\d+) .+ maxSize=(\d+)\)\)\n",
+                           config_map, flags=re.M)
         for i in pools:
-            target.labels(i[0]).set(int(i[1]))
-            maxsize.labels(i[0]).set(int(i[2]))
+            ready.labels(i[0]).set(int(i[1]))
+            target.labels(i[0]).set(int(i[2]))
+            maxsize.labels(i[0]).set(int(i[3]))
 
     except ApiException as e:
         if e.status != 404:  # no config map is not an error, it just means autoscaling is not enabled
@@ -24,7 +26,8 @@ except config.ConfigException:
     config.load_kube_config()
 
 v1 = client.CoreV1Api()
-target = Gauge('cluster_autoscaler_pool', 'cloudProviderTarget', ['name'])
+ready = Gauge('cluster_autoscaler_pool', 'ready', ['name'])
+target = Gauge('cluster_autoscaler_pool_target', 'cloudProviderTarget', ['name'])
 maxsize = Gauge('cluster_autoscaler_pool_max', 'maxSize', ['name'])
 
 start_http_server(9100)
